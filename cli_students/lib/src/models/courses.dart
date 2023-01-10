53c3913bbc68ba548cc87506cbe766e5f3d6a0f5
Davi Severom <severosantana2014@gmail.com>
2023-01-09 22:13:50 -0300

    Neste commit, terminei de criar a serialização e deserialização dos modelos

--- cli_students/lib/src/models/courses.dart
+++ cli_students/lib/src/models/courses.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Courses {
   int id;
   String name;
@@ -9,6 +11,23 @@ class Courses {
     required this.isStudents,
   });
 
-  
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'isStudents': isStudents,
+    };
+  }
 
+  String toJson() => jsonEncode(toMap());
+
+  factory Courses.fromMap(Map<String, dynamic> map) {
+    return Courses(
+      id: map['id'] ?? 0,
+      name: map['name'] ?? '',
+      isStudents: map['isStudents'] ?? false,
+    );
+  }
+
+  factory Courses.fromJson(String json) => Courses.fromMap(jsonDecode(json));
 }
--- cli_students/lib/src/models/students.dart
+++ cli_students/lib/src/models/students.dart
@@ -1,15 +1,58 @@
+import 'dart:convert';
+import 'dart:indexed_db';
+import 'dart:svg';
+
+import 'address.dart';
+import 'courses.dart';
+
 class Students {
-  int id;
-  String name;
-  int? age;
-  String nameCourses;
+  final int? id;
+  final String name;
+  final int? age;
+  final List<String> nameCourses;
+  final List<Courses> courses;
+  final Addres addres;
 
   Students({
-    required this.id,
+    this.id,
     required this.name,
     this.age,
     required this.nameCourses,
+    required this.courses,
+    required this.addres,
   });
 
-  
+  Map<String, dynamic> toMap() {
+    final map = <String, dynamic>{
+      'id': id,
+      'name': name,
+      'nameCourses': nameCourses,
+      'courses': courses.map((curso) => curso.toMap()).toList(),
+      'address': addres.toMap(),
+    };
+
+    if (age != null) {
+      map['age'] = age;
+    }
+
+    return map;
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Students.fromMap(Map<String, dynamic> map) {
+    return Students(
+      id: map['id'] ?? 0,
+      name: map['name'] ?? '',
+      age: map['age'],
+      nameCourses: List<String>.from((map['nameCourses'] ?? <String>[])),
+      addres: Addres.fromMap(map['address'] ?? <String, dynamic>{}),
+      courses: map['courses']
+              ?.map<Courses>((cursoMap) => Courses.fromMap(cursoMap))
+              .toList() ??
+          <Courses>[],
+    );
+  }
+
+  factory Students.fromJson(String json) => Students.fromMap(jsonDecode(json));
 }
